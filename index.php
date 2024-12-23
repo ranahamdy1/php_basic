4d4c1409731fa2370fa25712395c18391159e8d5
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-12-23 13:43:24 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php_basic</title>
+</head>
+<body>
+<h1>hello world</h1>
+<p>
+    <?php
+    echo "hello world";
+    ?>
+</p>
+<p>
+    <?php
+    //creat variable
+    $var = 10;
+    echo $var;
+    ?>
+</p>
+<p>
+    <?php
+    echo '$var'; // Display as a string
+    ?>
+</p>
+<p>
+    <?php
+    echo "$var"; // Display the value of $var
+    ?>
+</p>
+</body>
+</html>
